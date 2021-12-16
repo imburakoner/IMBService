2706f7fdf98623c237777a134da2f03b73f43532
Burak ÖNER <imburakoner@gmail.com>
2021-12-16 20:51:32 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IMBService
+
+A description of this package.
---
+++ Tests/IMBServiceTests/IMBServiceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import IMBService
+
+final class IMBServiceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(IMBService().text, "Hello, World!")
+    }
+}
